5f3d7be8ee85bb0c71417aaa2bec9d463b80f6a1
Felipe <felipesousabr2030@gmail.com>
2025-07-11 07:56:04 -0300

    Create LetraA.c

---
+++ LetraA.c
@@ -0,0 +1,44 @@
+Letra a
+#include <stdio.h>
+
+int main() {
+    
+    printf("**********\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("**********\n");
+
+    printf(" *** \n");
+    printf("*   *\n");
+    printf("*   *\n");
+    printf("*   *\n");
+    printf("*   *\n");
+    printf("*   *\n");
+    printf(" *** \n");
+
+    
+    printf("    *\n");
+    printf("   ***\n");
+    printf("  *****\n");
+    printf(" *******\n");
+    printf("*********\n");
+    printf("   *\n");
+    printf("   *\n");
+
+    // Losango
+    printf("-------------\n");
+    printf("      *\n");
+    printf("    *  *\n");
+    printf("   *    *\n");
+    printf("  *      *\n");
+    printf("   *    *\n");
+    printf("    *  *\n");
+    printf("      *\n");
+
+    return 0;
+}
